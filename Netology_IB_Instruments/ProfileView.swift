5e4b03352262831d56bc47a4d5f9e1668d9c4787
Nikolay Dobychin <imkoddi@mail.ru>
2022-02-25 12:57:37 +0300

    created ProfileView.swift and added @IBOoutlets

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by KODDER on 25.02.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthLabel: UILabel!
+    @IBOutlet weak var placeLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var pictureView: UIImageView!
+    
+}
