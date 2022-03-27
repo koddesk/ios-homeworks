10c8fe80ec5fc0a5fb5cedf2d172c1b34f5060eb
Nikolay Dobychin <imkoddi@mail.ru>
2022-03-27 18:48:05 +0300

    1.  Создан ProfileHeaderView.swift, добавлены  UI-элементы и обработка нажатия по кнопке addTarget 2. В ProfileViewController.swift создан экземпляр класса ProfileHeaderView и добавлен в качестве subview

--- Netology_IB_Instruments/Profile/ProfileViewController.swift
+++ Netology_IB_Instruments/Profile/ProfileViewController.swift
@@ -8,13 +8,17 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
+    let profileHeaderView = ProfileHeaderView(frame: .zero)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       
+        view.backgroundColor = .lightGray
+        view.addSubview(profileHeaderView)
     }
     
-
-
+    override func viewWillLayoutSubviews() {
+        profileHeaderView.frame = view.frame
+    }
 }
--- Netology_IB_Instruments/TabBarController.swift
+++ Netology_IB_Instruments/TabBarController.swift
@@ -8,30 +8,40 @@
 import UIKit
 
 class TabBarController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         UITabBar.appearance().barTintColor = .systemBackground
         tabBar.tintColor = .blue
-           setupVCs()
+        tabBar.backgroundColor = .white
+        
+        setupVCs()
     }
     func setupVCs() {
-           viewControllers = [
-               createNavController(for: FeedViewController(), title: NSLocalizedString("Лента", comment: ""), image: UIImage(systemName: "house")!),
-               createNavController(for: ProfileViewController(), title: NSLocalizedString("Профиль", comment: ""), image: UIImage(systemName: "person")!)
-           ]
-       }
-
+        viewControllers = [
+            createNavController(for: FeedViewController(), title: NSLocalizedString("Лента", comment: ""), image: UIImage(systemName: "house")!),
+            createNavController(for: ProfileViewController(), title: NSLocalizedString("Профиль", comment: ""), image: UIImage(systemName: "person")!)
+        ]
+    }
+    
     fileprivate func createNavController(for rootViewController: UIViewController,
-                                                      title: String,
-                                                      image: UIImage) -> UIViewController {
-            let navController = UINavigationController(rootViewController: rootViewController)
-            navController.tabBarItem.title = title
-            navController.tabBarItem.image = image
-            navController.navigationBar.prefersLargeTitles = true
-            rootViewController.navigationItem.title = title
-            return navController
-        }
+                                         title: String,
+                                         image: UIImage) -> UIViewController {
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .white
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.black]
+        
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        navController.navigationBar.prefersLargeTitles = false
+        navController.navigationBar.tintColor = .blue
+        navController.navigationBar.standardAppearance = appearance
+        navController.navigationBar.compactAppearance = appearance
+        navController.navigationBar.scrollEdgeAppearance = appearance
+        rootViewController.navigationItem.title = title
+        return navController
+    }
     
 }
