10774b44526d931dc56a2ca3784f4d7bc7f162c0
Nikolay Dobychin <imkoddi@mail.ru>
2022-04-13 13:25:24 +0300

    Создан PhotosViewController.swift -> в него добавлен экземпляр класса UICollectionView

---
+++ Netology_IB_Instruments/Profile/PhotosCollection.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosCollection.swift
+//  Netology_IB_Instruments
+//
+//  Created by KODDER on 13.04.2022.
+//
+
+import UIKit
+
+struct PhotosCollectionView {
+    
+    var image:String = ""
+    
+    init(image: String){
+        self.image = image
+    }
+}
